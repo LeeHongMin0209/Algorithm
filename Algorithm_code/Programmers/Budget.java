e6e6ed93a7373bea706d6f42a47dda6e834156df
LeeHongMin0209 <64480971+LeeHongMin0209@users.noreply.github.com>
2020-05-23 03:39:40 +0900

    예산

---
+++ Algorithm_code/Programmers/Budget.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Budget {
+	public int solution(int[] d, int budget) {
+		int answer = 0;
+		
+		Arrays.sort(d);
+		for(int i = 0; i < d.length; i++) {
+			if(d[i] <= budget) {
+				budget -= d[i];
+				answer++;
+			}
+			if(budget == 0 || budget < d[i]) break;
+		}
+		
+		return answer;
+	}
+}

d01ed18bfd543dacc9dec7c1f84e93fbbba658ba
LeeHongMin0209 <64480971+LeeHongMin0209@users.noreply.github.com>
2020-06-01 08:51:12 +0900

    11727번 : 타일링2

--- Algorithm_code/BOJ/BOJ11727.java
+++ Algorithm_code/BOJ/BOJ11727.java
@@ -1,10 +1,20 @@
 package BOJ;
 
+import java.util.Scanner;
+
 public class BOJ11727 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 3;
+		for(int i = 3; i <= n; i++) {
+			dp[i] = (dp[i-2] * 2 + dp[i-1]) % 10007;
+		}
+		System.out.println(dp[n]);
 	}
 
 }

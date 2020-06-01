ec295f6a58dad9d81480400af2eee0d68116ad2e
LeeHongMin0209 <64480971+LeeHongMin0209@users.noreply.github.com>
2020-06-01 09:36:45 +0900

    12865번 : 평범한 배낭

---
+++ Algorithm_code/BOJ/BOJ12865.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ12865 {
+	static int n,k;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[][] dp = new int[n+1][k+1];
+
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			for(int j = 1; j <= k; j++) {
+				if(j < w) {
+					dp[i][j] = dp[i-1][j];
+				}
+				else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w] + v);
+				}
+			}
+		}
+		System.out.println(dp[n][k]);
+	}
+
+}

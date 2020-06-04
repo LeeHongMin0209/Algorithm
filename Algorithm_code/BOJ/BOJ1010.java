7351ecac82862e0a9e75d38e8d7862bb40a27707
LeeHongMin0209 <64480971+LeeHongMin0209@users.noreply.github.com>
2020-06-04 15:49:32 +0900

    1010번 : 다리놓기

---
+++ Algorithm_code/BOJ/BOJ1010.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ1010 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] dp = new int[31][31];
+		dp[1][0] = 1;
+		dp[1][1] = 1;
+		for(int i = 2; i <= 30; i++){
+			for(int j = 1; j <= 30; j++) {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+			}
+		}
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			
+			System.out.println(dp[m][n]);
+		}
+
+	}
+
+}

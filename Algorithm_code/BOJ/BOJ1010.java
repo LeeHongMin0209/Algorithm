6aa266d32b0294b29a7602f830a80abf5c35b1d7
LeeHongMin0209 <64480971+LeeHongMin0209@users.noreply.github.com>
2020-06-04 15:49:49 +0900

    1010번 : 다리놓기

--- Algorithm_code/BOJ/BOJ1010.java
+++ Algorithm_code/BOJ/BOJ1010.java
@@ -11,6 +11,7 @@ public class BOJ1010 {
 		dp[1][0] = 1;
 		dp[1][1] = 1;
 		for(int i = 2; i <= 30; i++){
+			dp[i][0] = 1;
 			for(int j = 1; j <= 30; j++) {
 				dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
 			}

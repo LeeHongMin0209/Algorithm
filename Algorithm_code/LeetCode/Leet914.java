1dd80cd3df9239109837268dab0d9a30eb1d1dc2
LeeHongMin0209 <64480971+LeeHongMin0209@users.noreply.github.com>
2020-06-11 00:19:47 +0900

    X of a Kind in a Deck of Cards

--- Algorithm_code/LeetCode/Leet914.java
+++ Algorithm_code/LeetCode/Leet914.java
@@ -1,7 +1,28 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leet914 {
 	public boolean hasGroupsSizeX(int[] deck) {
+		int N = deck.length;
+		int[] count = new int[10000];
+		for (int c: deck)
+			count[c]++;
 		
+		List<Integer> values = new ArrayList<Integer>();
+		for (int i = 0; i < 10000; ++i)
+			if (count[i] > 0)
+				values.add(count[i]);
+		
+		search: for (int X = 2; X <= N; ++X)
+			if (N % X == 0) {
+				for (int v: values)
+					if (v % X != 0)
+						continue search;
+				return true;
+			}
+		
+		return false;
 	}
 }

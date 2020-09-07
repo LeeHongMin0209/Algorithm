d0d83e615d6df934ab67a7c747fa8ea89373cd89
LeeHongMin <tmxkzm35@naver.com>
2020-09-08 02:20:19 +0900

    오픈채팅방

--- Algorithm_code/Programmers/OpenChatting.java
+++ Algorithm_code/Programmers/OpenChatting.java
@@ -7,38 +7,35 @@ import java.util.Vector;
 
 public class OpenChatting {
 	 static public String[] solution(String[] record) {
-	        HashMap<String, String> user = new HashMap<>();
-	        String[] logState = new String[100000];
-	        String[] logId = new String[100000];
-	        int j = 0;
-	        for(int i = 0; i < record.length; i++) {
-	        	String check = record[i];
-	        	StringTokenizer st = new StringTokenizer(check, " ");
-	        	String command = st.nextToken();
-	        	if(command.equals("Enter")) {
-	        		String id = st.nextToken();
-	        		String nick = st.nextToken();
-	        		user.put(id, nick);
-	        		logId[j] = id;
-	        		logState[j] = "´ÔÀÌ µé¾î¿Ô½À´Ï´Ù.";
-	        		j++;
-	        	} else if(command.equals("Leave")) {
-	        		String id = st.nextToken();
-	        		logId[j] = id;
-	        		logState[j] = "´ÔÀÌ ³ª°¬½À´Ï´Ù.";
-	        		j++;
-	        	} else if(command.equals("Change")){
-	        		String id = st.nextToken();
-	        		String nick = st.nextToken();
-	        		user.put(id, nick);
-	        	}
-	        }
-	        String answer[] = new String[j];
-	        for(int i = 0; i < j ; i++) {
-	        	answer[i] = user.get(logId[i])+logState[i];
-	        }
-	        
-	        return answer;
+	       HashMap<String, String> user = new HashMap<>();
+	       String[] logId = new String[100000];
+	       String[] logState = new String[100000];
+	       int cnt = 0;
+	       for(int i = 0; i < record.length; i++) {
+	    	   StringTokenizer st = new StringTokenizer(record[i]);
+	    	   String command = st.nextToken();
+	    	   if(command.equals("Enter")) {
+	    		   logId[cnt] = st.nextToken();
+	    		   logState[cnt] = "´ÔÀÌ µé¾î¿Ô½À´Ï´Ù.";
+	    		   String nick = st.nextToken();
+	    		   user.put(logId[cnt], nick);
+	    		   cnt++;
+	    	   } else if(command.equals("Leave")) {
+	    		   logId[cnt] = st.nextToken();
+	    		   logState[cnt] = "´ÔÀÌ ³ª°¬½À´Ï´Ù.";
+	    		   cnt++;
+	    	   } else if(command.equals("Change")) {
+	    		   String id = st.nextToken();
+	    		   String nick = st.nextToken();
+	    		   user.put(id, nick);
+	    	   }
+	       }
+	       String[] answer = new String[cnt];
+	       for(int i = 0; i < cnt ; i++) {
+	    	   String nick = user.get(logId[i]);
+	    	   answer[i] = nick + logState[i];
+	       }
+	       return answer;
 	    }
 	 public static void main(String[] args) {
 		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan","Enter uid1235 asdasd","Enter uid1225 xrodo"};

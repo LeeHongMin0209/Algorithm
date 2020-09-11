1648d324897dea8c5a8f3bfe10d77177365404ff
LeeHongMin <tmxkzm35@naver.com>
2020-09-11 13:46:45 +0900

    자물쇠와 열쇠

---
+++ Algorithm_code/Programmers/LockAndKey.java
@@ -0,0 +1,76 @@
+package Programmers;
+
+public class LockAndKey {
+	static boolean isOk = false;
+	
+	static public boolean solution(int[][] key, int[][] lock) {
+		int len = lock.length;
+		int[][] copyLock = new int[len*3][len*3];
+		for(int i = 0; i < len; i++) {
+			for(int j = 0; j < len; j++) {
+				copyLock[i+len][j+len] = lock[i][j];
+			}
+		}
+		dfs(key,copyLock, 0);
+		return isOk;
+	}
+	static void dfs(int[][] key, int[][] lock, int cnt) {
+		check(key,lock,0,0);
+		if(isOk) return;
+		if(cnt >= 1) return;
+		int[][] temp = rotate(key);
+		dfs(temp, lock, cnt+1);
+	}
+	static void check(int[][] key, int[][] lock, int x, int y) {
+		if(isOk) return;
+		if(y+key.length > lock.length) {
+			y = 0;
+			x++;
+		}
+		if(x+key.length > lock.length) return;
+
+		int[][] copyLock = new int[lock.length][lock.length];
+		for(int i = 0; i < lock.length; i++) {
+			copyLock[i] = lock[i].clone();
+		}
+		boolean isFail = false;
+		loop : for(int i = 0; i < key.length; i++) {
+			for(int j = 0; j < key.length; j++) {
+				if(key[i][j] == 1) {
+					if(copyLock[i+x][j+y] == 1) {
+						isFail = true;
+						break loop;
+					}
+					copyLock[i+x][j+y] = key[i][j];
+				}
+			}					
+		}
+		if(!isFail) {
+			loop : for(int i = 0; i < lock.length/3; i++) {
+				for(int j = 0; j < lock.length/3; j++) {
+					if(copyLock[i+lock.length/3][j+lock.length/3] != 1) {  
+						isFail = true;
+						break loop;
+					}
+				}
+			}
+		}
+		if(!isFail) isOk = true;
+		check(key, lock, x, y+1);
+	}
+	static int[][] rotate(int[][] key) {
+		int len = key.length;
+		int[][] temp = new int[len][len];
+		for(int i=0; i<len; i++) {
+			for(int j=0; j<len; j++) {
+				temp[i][j] = key[len-j-1][i];
+			}
+		}
+		return temp;
+	}
+	public static void main(String[] args) {
+		int[][] key = {{0,0,0},{1,0,0},{0,1,1}};
+		int[][] lock = {{1,1,1},{1,1,0},{1,0,1}};
+		boolean ans = solution(key, lock);
+	}
+}

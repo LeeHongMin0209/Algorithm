206709366aa3f4b35d1bf35386a656f944aa6aa8
LeeHongMin <tmxkzm35@naver.com>
2020-09-15 14:27:14 +0900

    11650번 : 좌표 정렬하기

---
+++ Algorithm_code/BOJ/BOJ11650.java
@@ -0,0 +1,52 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ11650 {
+	static class dot{
+		int x;
+		int y;
+		public dot(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		dot[] list = new dot[n];
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			dot d = new dot(x, y);
+			list[i] = d;
+		}
+		 Arrays.sort(list, new Comparator<dot>() {
+	        	public int compare(dot o1, dot o2) {
+	                if(o1.x != o2.x) {
+	                	//Double 인수 정렬할때는 다음과 같이
+	                    return (o1.x - o2.x);
+	                }
+	                //int 인수 정렬은 다음과 같이
+	                return o1.y - o2.y;
+	            }
+	        });
+		StringBuilder sb =  new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			int x = list[i].x;
+			int y = list[i].y;
+			sb.append(x +" "+ y + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+}

860197c11d6e1dc6cad0ea2081ebf26c3d224937
LeeHongMin <tmxkzm35@naver.com>
2020-09-20 15:05:57 +0900

    test

---
+++ Algorithm_code/BOJ/BOJ6603.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ6603 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}

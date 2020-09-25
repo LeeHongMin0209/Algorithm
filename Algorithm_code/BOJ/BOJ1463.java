35abf95f05a0a70579a70802452112b71b3004f9
LeeHongMin <tmxkzm35@naver.com>
2020-09-25 23:19:32 +0900

    1463번 : 1로 만들기

--- Algorithm_code/BOJ/BOJ1463.java
+++ Algorithm_code/BOJ/BOJ1463.java
@@ -1,21 +1,24 @@
 package BOJ;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ1463 {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int[] dp = new int[1000001];
-		dp[1] = 0;
-		for(int i = 2; i <= n; i++) {
-			dp[i] = dp[i-1] + 1;
-			if(i % 3 == 0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
-			if(i % 2 == 0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
+	static int[] arr = new int[10000000];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr[1] = 0;
+		arr[2] = 1;
+		arr[3] = 1;
+		for(int i = 4; i <= n; i++) {
+			if(i % 2 == 0) arr[i] = Math.min(arr[i / 2] + 1, arr[i-1] + 1);
+			if(i % 3 == 0) arr[i] = Math.min(arr[i / 3] + 1, arr[i-1] + 1);
+			if(i%2 != 0 && i%3 != 0) arr[i] = arr[i-1] + 1;
+			
 		}
-		System.out.println(dp[n]);
+		System.out.println(arr[n]);
 	}
 
 }

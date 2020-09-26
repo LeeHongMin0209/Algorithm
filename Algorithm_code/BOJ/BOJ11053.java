57e078a722875b5e5bce5bd4b8a714cef3342d1b
LeeHongMin <tmxkzm35@naver.com>
2020-09-26 16:50:10 +0900

    11053번 : 가장 긴 증가하는 부분수열

--- Algorithm_code/BOJ/BOJ11053.java
+++ Algorithm_code/BOJ/BOJ11053.java
@@ -9,22 +9,25 @@ public class BOJ11053 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		int[] arr = new int[1002];
+		int[] dp = new int[1002];
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[1000];
-		int[] dp = new int[1000];
-		int result = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
+		for(int i = 1 ; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		for(int i = 0; i < n; i++) {
+		dp[1] = 1;
+		int max = arr[1];
+		for(int i = 2; i <= n; i++) {
 			dp[i] = 1;
-			for(int j = 0; j < i; j++) {
-				if(arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j]+1);
+			for(int j = 1; j < i; j++) {
+				if(arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j]+1);
 			}
 		}
-		System.out.println(dp[n-1]);
+		max = 0;
+		for(int i = 1; i <= n; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
 	}
-
 }

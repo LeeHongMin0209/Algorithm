578f16c348f91a93c9a305b9f53d353e1759d541
LeeHongMin <tmxkzm35@naver.com>
2020-09-26 16:23:19 +0900

    1912번 : 연속합

--- Algorithm_code/BOJ/BOJ1912.java
+++ Algorithm_code/BOJ/BOJ1912.java
@@ -9,26 +9,22 @@ public class BOJ1912 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n+1];
-		int[] dp = new int[n+1];
-
+		int[] arr = new int[100002];
+		int[] dp = new int[100002];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		dp[1] = arr[1];
-		int max = arr[1];
 		for(int i = 2; i <= n; i++) {
-			dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
-			max = Math.max(max, dp[i]);
-			
+			dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
 		}
+		int ans = Integer.MIN_VALUE;
 		for(int i = 1; i <= n; i++) {
-			System.out.println(dp[i]);
+			ans = Math.max(dp[i], ans);
 		}
-		System.out.println(max);
+		System.out.println(ans);
 	}
 
 }

17adae1a80162ed22cb12aadf597995849a49177
LeeHongMin <tmxkzm35@naver.com>
2020-09-26 20:42:08 +0900

    9095번 : 1,2,3 더하기

---
+++ Algorithm_code/BOJ/BOJ9095.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9095 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t --> 0) {
+			int n = Integer.parseInt(br.readLine());
+			int[] dp = new int[12];
+			dp[1] = 1;
+			dp[2] = 2;
+			dp[3] = 4;
+			for(int i = 4; i <= n; i++) {
+				dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+			}
+			System.out.println(dp[n]);
+		}
+
+	}
+
+}

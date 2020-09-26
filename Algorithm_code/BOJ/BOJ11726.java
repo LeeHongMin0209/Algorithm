26fe0a0ff6b59b9d038c25c87b5b1e1db1f855e8
LeeHongMin <tmxkzm35@naver.com>
2020-09-26 15:59:37 +0900

    11726번 : 2xn 타일링

--- Algorithm_code/BOJ/BOJ11726.java
+++ Algorithm_code/BOJ/BOJ11726.java
@@ -1,20 +1,21 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BOJ11726 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		int[] dp = new int[1001];
 		dp[1] = 1;
 		dp[2] = 2;
 		for(int i = 3; i <= n; i++) {
-			dp[i] = dp[i-1]+dp[i-2] % 10007;
+			dp[i] = dp[i-1] + dp[i-2] % 10007;
 		}
-		System.out.println(dp[n]);
+		System.out.println(dp[n] % 10007);
 	}
-
 }

86043bfbee38c07b2065e2f07687d06360b3f42f
LeeHongMin <tmxkzm35@naver.com>
2020-10-02 22:19:51 +0900

    스킬트리

--- Algorithm_code/Programmers/SkillTree.java
+++ Algorithm_code/Programmers/SkillTree.java
@@ -1,27 +1,27 @@
 package Programmers;
 
 public class SkillTree {
-	
-	 public int solution(String skill, String[] skill_trees) {
-		 int answer = 0;
-		 for(int i = 0; i < skill_trees.length; i++) {
-			 boolean check = true;
-			 int idx = 0;
-			 for(char curCh : skill_trees[i].toCharArray()) {
-				 int temp = skill.indexOf(curCh);
-				 
-				 if(temp == -1) continue;	        		
-				 else if(temp == idx) {
-					 idx++;
-				 }else {
-					 check = false;
-					 break;
-				 }	        		
-			 }
-			 if(check) answer++;
-		 }
-		 return answer;
-	 }
+	public int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+		for(int i = 0; i < skill_trees.length; i++) {
+			boolean check = check_skill(skill, skill_trees[i]);
+			if(check) answer++;
+		}
+		return answer;
+	} 
+	static boolean check_skill(String skill, String skill_tree) {
+		char[] arr1 = skill.toCharArray();
+		char[] arr2 = skill_tree.toCharArray();
+		int idx = 0;
+		for(int i = 0; i < arr2.length; i++) {
+			char ch = arr2[i];
+			for(int j = 0; j < arr1.length; j++) {
+				if(arr1[j] == arr2[i] && idx != j) return false;
+				else if(arr1[j] == arr2[i] && idx == j) idx++;  
+			}
+		}
+		return true;
+	}
 }
 
 

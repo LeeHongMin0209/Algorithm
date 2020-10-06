bc9d16d620f8a1b94f081068fc91fc1b4e7149d9
LeeHongMin <tmxkzm35@naver.com>
2020-10-06 22:24:27 +0900

    11404번 : 플로이드

---
+++ Algorithm_code/BOJ/BOJ11404.java
@@ -0,0 +1,43 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11404 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[][] map = new int[n+1][n+1];
+		for(int i = 1; i <= n; i++) {
+			Arrays.fill(map[i], 10000001);			
+		}
+		for(int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			map[a][b] = Math.min(map[a][b], c);
+		}
+		for(int i = 1; i <= n ; i++) map[i][i] = 0;
+		for(int k = 1; k <= n; k++) {
+			for(int i = 1; i <= n; i++) {
+				for(int j = 1; j <= n; j++) {
+					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+				}
+			}
+		}
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(map[i][j] >= 10000001) System.out.println("0 ");
+				else System.out.println(map[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}

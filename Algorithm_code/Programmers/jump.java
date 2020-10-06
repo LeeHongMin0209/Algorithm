ded524765a33e553aa0d7a955880d5bf04b12f73
LeeHongMin <tmxkzm35@naver.com>
2020-10-06 18:02:58 +0900

    점프와 순간이동

---
+++ Algorithm_code/Programmers/jump.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+public class jump {
+	static public int solution(int n) {
+		int ans = 0;
+		while(n != 0) {
+			if(n % 2 == 0) {
+				n /= 2;
+			} else {
+				n--;
+				ans++;
+			}
+		}
+		return ans;
+    }
+}

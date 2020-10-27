f5ce53f678e11876fc6ebd65ebb425c74c68d8d9
LeeHongMin <tmxkzm35@naver.com>
2020-10-27 19:34:15 +0900

    1543번 : 문서 검색

--- Algorithm_code/BOJ/BOJ1543.java
+++ Algorithm_code/BOJ/BOJ1543.java
@@ -8,7 +8,6 @@ public class BOJ1543 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int ans = 0;
 		String document = br.readLine();
 		String search = br.readLine();

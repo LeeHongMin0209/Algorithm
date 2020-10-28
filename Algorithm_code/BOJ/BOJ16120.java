879b774d40abffc499b1c5cd44c5b6e5ea4ed9f3
LeeHongMin <tmxkzm35@naver.com>
2020-10-28 23:41:16 +0900

    16120번

--- Algorithm_code/BOJ/BOJ16120.java
+++ Algorithm_code/BOJ/BOJ16120.java
@@ -1,9 +1,15 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ16120 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
 
 	}
 

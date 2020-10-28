9241645ab111d318d2ce7b0addb6e2bd6ffe179d
LeeHongMin <tmxkzm35@naver.com>
2020-10-29 00:56:55 +0900

    16120번 : PPAP

--- Algorithm_code/BOJ/BOJ16120.java
+++ Algorithm_code/BOJ/BOJ16120.java
@@ -10,7 +10,32 @@ public class BOJ16120 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = br.readLine();
-
+		String cmp = "PPAP";
+		boolean last = false;
+		boolean PPAP = true;
+		int one = 0;
+		
+		for(int i = 0; i < str.length(); i++){
+			if(str.charAt(i) == 'P'){
+				one++;
+				continue; //esle안쓰고 -> 시간 줄이기 위해
+			}
+			if(i != str.length() - 1) {
+				if(one >=2 && str.charAt(i+1) == 'P'){
+					one--;
+					i++;
+				}
+				else {
+					PPAP = false;
+					break;
+				}
+			} else {
+				if(str.charAt(i) == 'P' && str.charAt(i-1) != 'A' || str.charAt(i) == 'A') PPAP = false;
+			}
+		}
+		
+		if(one == 1 && PPAP) System.out.println("PPAP");
+		else System.out.println("NP");
 	}
 
 }

005c94ad5c1dbeb146ec32739df949ccc9a8f01f
hongmin <tmxkzm35@naver.com>
2020-11-11 21:20:13 +0900

    1541번 : 잃어버린 괄호

--- Algorithm_code/BOJ/BOJ1541.java
+++ Algorithm_code/BOJ/BOJ1541.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ1541 {
-
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();

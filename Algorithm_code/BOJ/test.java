61e37de9646567a599c6cc0b1065e20697b55a49
hongmin <tmxkzm35@naver.com>
2020-11-20 19:05:19 +0900

    testcommit

--- Algorithm_code/BOJ/test.java
+++ Algorithm_code/BOJ/test.java
@@ -1,44 +1,64 @@
 package BOJ;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'findNumber' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER k
+     */
+
+    public static String findNumber(List<Integer> arr, int k) {
+    // Write your code here
+        if(arr.contains(k)) return "YES";
+        else return "NO";
+    }
+
+}
 
 public class test {
-	static ArrayList<Integer> LIS = new ArrayList<>();
-	static int lower_idx(int target, int len) {
-		int st = 0;
-		int en = len;
-		while(st < en) {
-			int mid = (st+en) / 2;
-			if(LIS.get(mid) >= target) en = mid;
-			else st = mid + 1;
-		}
-		return st;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[1002];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		LIS.add(arr[0]);
-		for(int i = 1; i < n; i++) {
-			int num = arr[i];
-			int idx = lower_idx(num, LIS.size());
-			if(idx >= LIS.size()) {
-				LIS.add(num);
-			} else LIS.set(idx, num);
-			
-		}
-		System.out.println(LIS.size());
-	}
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.findNumber(arr, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
